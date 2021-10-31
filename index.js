8f3226ed628630efb9fe347889cac0cc8f06deeb
subir2 <subirsarker1995@gmail.com>
2021-10-31 08:54:00 +0600

    added port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 
 const app=express();
-const port=5000;
+const port=process.env.PORT||5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nw8x5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,6 +17,10 @@ app.use(express.json());
 // app.get("/",(req,res)=>{
 // console.log("Hello Its Running");
 // });
+app.get('/',(req,res)=>{
+ res.send("Running Own Server");
+  //console.log("Running Own Server");
+});
 
 client.connect(err => {
   const productCollection = client.db("tourism").collection("products");
